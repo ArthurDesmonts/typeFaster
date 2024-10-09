f40f4c795290258068618675a527b994b2804e15
arthur <arthur.desmonts@gmail.com>
2024-10-09 17:07:37 +0200

    initial commit : install dependencies for tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import '@/assets/tailwind.css'
+import NavBar from '@/components/NavBar.vue'
+
+</script>
+
+<template>
+  <div>
+    <NavBar/>
+  </div>
+  <div>
+    <h1 class="text-4xl text-center">En construction !</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
