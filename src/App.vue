115d402e41db307562bdba383b9dd6eefa3dc399
arthur <arthur.desmonts@gmail.com>
2024-10-10 18:50:18 +0200

    fix ROOT css elements, remove bad imports

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
 import '@/assets/tailwind.css'
-import TextComponent from '@/components/TextComponent.vue'
 import NavBar from "@/components/NavBar.vue";
 
 </script>
 
 <template>
-  <div class="flex bg-gray-700">
+  <div class="flex bg-gray-700 text-gray-100">
     <NavBar></NavBar>
     <div class="flex-grow">
       <RouterView></RouterView>
