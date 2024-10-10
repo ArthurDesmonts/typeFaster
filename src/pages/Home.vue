adfed64295b12bd97c8a8ea314bacff86f40a69e
arthur <arthur.desmonts@gmail.com>
2024-10-10 13:39:35 +0200

    Fix title HomePage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -6,7 +6,11 @@ import TextComponent from "@/components/TextComponent.vue";
 <template>
   <div class="bg-gray-700 h-screen">
     <div id="top-part" class="container mx-auto">
-      <h1 class="text-yellow-600 font-bold text-4xl justify-center uppercase text-center p-10">Type Faster</h1>
+        <h1 class="text-5xl font-extrabold justify-center uppercase text-center p-10">
+          <span class="bg-clip-text text-transparent bg-gradient-to-r from-yellow-600 to-orange-500">
+            Type Faster
+          </span>
+        </h1>
     </div>
     <div id="main-content" class="container mx-auto px-4">
       <TextComponent/>
