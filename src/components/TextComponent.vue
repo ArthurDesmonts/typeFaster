49f4accfa4925bacfc65d751c18441c5a41fd2df
arthur <arthur.desmonts@gmail.com>
2024-10-14 14:52:31 +0200

    add reset countdown + macro to reset the text

--- src/components/TextComponent.vue
+++ src/components/TextComponent.vue
@@ -3,7 +3,7 @@ import { onMounted, onUnmounted, ref } from "vue";
 import axios from "axios";
 import { receiverText, receiverIDText } from "@/utils/objectPreTreatmentReceiver.js";
 import { initializer, textToTab, cleanString } from "@/utils/TextTreatment.js";
-import { countDownFrom } from "@/utils/TimeHandler.js";
+import { countDownFrom, resetCountDown } from "@/utils/TimeHandler.js";
 
 let rawText = ref("");
 let textDisplayed = ref("");
@@ -16,7 +16,6 @@ const typedWords = ref([]);
 const typedWordsLenght = ref(0);
 
 const timer = ref(30);
-const timerDefault = timer;
 const wpm = ref(0);
 
 const input = ref("");
@@ -34,6 +33,7 @@ async function getTextFromServer() {
   }
 }
 
+// Function to reset variables
 const resetVariables = () => {
   currentWordIndex.value = 0;
   currentWordToType.value = textArray.value[currentWordIndex.value];
@@ -46,7 +46,6 @@ const resetVariables = () => {
 
 // Function to update the displayed text
 async function updateText() {
-  console.log("updateText");
   await getTextFromServer();
   if (rawText.value) {
     textDisplayed.value = initializer(rawText.value, 300);
@@ -59,26 +58,24 @@ async function updateText() {
   window.addEventListener("keydown", handleStartGame, { once: true });
   timer.value = 30;
 
-  //remove the focus on the button
+  // Remove the focus on the button
   updateButton.value.blur();
 }
 
+// Function to handle typing
 const typing = () => {
   if (cleanString(input.value) === currentWordToType.value) {
-    //add 1 to the wpm
     currentWordIndex.value++;
     currentWordToType.value = textArray.value[currentWordIndex.value];
-    //push for wpm
     typedWords.value.push(input.value);
     typedWordsLenght.value = typedWords.value.length;
-
-    //empty the field
     input.value = "";
   } else {
     console.log(input.value + " != "+ currentWordToType.value + " : NOPE !");
   }
 };
 
+// Function to handle countdown
 const countDown = () => {
   countDownFrom(timer.value, (remainingSeconds) => {
     timer.value = remainingSeconds;
@@ -88,6 +85,7 @@ const countDown = () => {
   });
 }
 
+// Function to block typing signal
 function blockTypingSignal() {
   input.value = "";
   window.removeEventListener("keydown", handleSpaceBarDown);
@@ -96,6 +94,7 @@ function blockTypingSignal() {
   summary.value = true;
 }
 
+// Function to highlight text
 function highlightText(array, currentIndex) {
   return array.map((word, index) => {
     if (index < currentIndex) {
@@ -105,23 +104,20 @@ function highlightText(array, currentIndex) {
   }).join(' ');
 }
 
+// Event handler for space bar down
 const handleSpaceBarDown = (event) => {
-  if (event.keyCode === 32) {// spacebar
+  if (event.keyCode === 32) {
     currentWordToType.value = cleanString(currentWordToType.value);
     typing();
   }
 };
 
-const handleKeyup = (event) => {
-  if (event.keyCode === 32) { // spacebar
-    typedWords.value.push(input.value);
-  }
-};
-
+// Event handler to start game
 const handleStartGame = () => {
   countDown();
 };
 
+// Event handler for typing letter key
 const handleTypingLetterKey = (event) => {
   if (event.keyCode <= 90 && event.keyCode >= 65) {
     input.value += event.key;
@@ -131,16 +127,26 @@ const handleTypingLetterKey = (event) => {
   }
 };
 
+//ctrl + r to refresh text
+const handleKeyRefreshText = (event) => {
+  if (event.ctrlKey && event.keyCode === 82) {
+    updateText();
+    resetCountDown();
+  }
+};
+
+// Lifecycle hook for component mount
 onMounted(() => {
   updateText();
+  window.addEventListener("keydown", handleKeyRefreshText);
   window.addEventListener("keydown", handleSpaceBarDown);
-  window.addEventListener("keydown", handleStartGame,  { once: true });
+  window.addEventListener("keydown", handleStartGame, {once: true});
   window.addEventListener("keydown", handleTypingLetterKey);
 });
 
+// Lifecycle hook for component unmount
 onUnmounted(() => {
   window.removeEventListener("keydown", handleSpaceBarDown);
-  window.removeEventListener("keyup", handleKeyup);
   window.removeEventListener("keydown", handleStartGame);
   window.removeEventListener("keydown", handleTypingLetterKey);
 });
@@ -150,10 +156,13 @@ onUnmounted(() => {
   <div class="flex flex-col justify-center">
     <p class="text-2xl p-4 my-4 rounded text-center text-customBlue-100">{{ timer }}</p>
     <div class="flex justify-center w-full">
-      <button ref="updateButton" type="button" class="bg-amber-400 text-black font-bold rounded px-2 rounded-r-none" @click="updateText">&#x21bb;
+      <button ref="updateButton" type="button" class="bg-amber-400 text-black font-bold rounded px-2 rounded-r-none"
+              @click="updateText">&#x21bb;
       </button>
-      <div class="rounded rounded-l-none bg-gray-950 max-w-xl min-w-[600px] min-h-[250px] max-h-[250px] overflow-hidden">
-        <p v-html="highlightText(textArray, currentWordIndex)" class="font-mono text-customBlue-100 text-justify p-2"></p>
+      <div
+          class="rounded rounded-l-none bg-gray-950 max-w-xl min-w-[600px] min-h-[250px] max-h-[250px] overflow-hidden">
+        <p v-html="highlightText(textArray, currentWordIndex)"
+           class="font-mono text-customBlue-100 text-justify p-2"></p>
       </div>
     </div>
     <div class="flex justify-center w-full">
