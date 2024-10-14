cfc2ffb6a8a177036d8cce6ed848d14d04b39f2f
arthur <arthur.desmonts@gmail.com>
2024-10-14 12:45:21 +0200

    remove the suppression of the word after typing for a gray mask on words correct

--- src/components/TextComponent.vue
+++ src/components/TextComponent.vue
@@ -33,6 +33,16 @@ async function getTextFromServer() {
   }
 }
 
+const resetVariables = () => {
+  currentWordIndex.value = 0;
+  currentWordToType.value = textArray.value[currentWordIndex.value];
+  input.value.disabled = false;
+  wpm.value = 0;
+  typedWordsLenght.value = 0;
+  typedWords.value = [];
+  summary.value = false;
+}
+
 // Function to update the displayed text
 async function updateText() {
   await getTextFromServer();
@@ -40,13 +50,8 @@ async function updateText() {
     textDisplayed.value = initializer(rawText.value, 300);
     textArray.value = textToTab(textDisplayed.value);
   }
-  //reset the game
-  currentWordIndex.value = 0;
-  currentWordToType.value = textArray.value[currentWordIndex.value];
-  input.value.disabled = false;
-  wpm.value = 0;
-  typedWordsLenght.value = 0;
-  summary.value = false;
+
+  resetVariables();
 
   window.addEventListener("keydown", handleKeydown);
   window.addEventListener("keydown", handleStartGame, { once: true });
@@ -92,6 +97,15 @@ function blockTypingSignal() {
   summary.value = true;
 }
 
+function highlightText(array, currentIndex) {
+  return array.map((word, index) => {
+    if (index < currentIndex) {
+      return `<span class="typed-word">${word}</span>`;
+    }
+    return word;
+  }).join(' ');
+}
+
 const handleKeydown = (event) => {
   if (event.keyCode === 32) {// spacebar
     currentWordToType.value = cleanString(currentWordToType.value);
@@ -132,7 +146,7 @@ onUnmounted(() => {
       <button class="bg-amber-400 text-black font-bold rounded px-2 rounded-r-none" @click="updateText">&#x21bb;
       </button>
       <div class="rounded rounded-l-none bg-gray-950 max-w-xl min-w-[600px] min-h-[250px] max-h-[250px] overflow-hidden">
-        <p v-html="textDisplayed" class="font-mono text-customBlue-100 text-justify p-2"></p>
+        <p v-html="highlightText(textArray, currentWordIndex)" class="font-mono text-customBlue-100 text-justify p-2"></p>
       </div>
     </div>
     <div class="flex justify-center w-full mt-5">
@@ -146,3 +160,9 @@ onUnmounted(() => {
     </div>
   </div>
 </template>
+
+<style>
+.typed-word {
+  color: gray;
+}
+</style>
