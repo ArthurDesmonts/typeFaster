17c5c7bcd914bf62f8c7ebe618c9fa3c37f1f117
arthur <arthur.desmonts@gmail.com>
2024-10-15 15:51:33 +0200

    ADD connection form UI

---
+++ src/components/signIn.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container bg-customBlue-900 flex justify-center rounded-2xl w-fit p-10 mx-auto mt-20">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <h1 class="text-center mb-1">Se connecter</h1>
+        <form>
+          <div class="flex flex-col gap-2">
+            <div class="mb-3 mt-2">
+              <input type="text" placeholder="Pseudo" class="form-control bg-customBlue-800 rounded font-bold text-center" id="name">
+            </div>
+            <div class="mb-3">
+              <input type="password" placeholder="Mot de passe" class="form-control bg-customBlue-800 rounded font-bold text-center" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary bg-customOrange-500 rounded text-customBlue-900 p-2">Connection</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+input::placeholder {
+  text-align: center;
+}
+</style>
--- src/pages/Profil.vue
+++ src/pages/Profil.vue
@@ -1,9 +1,14 @@
 <script setup>
-
 </script>
 
 <template>
-  <p class="text-center">Profil</p>
+  <div id="top-part" class="container mx-auto">
+    <h1 class="text-5xl font-extrabold justify-center uppercase text-center p-10">
+          <span class="bg-clip-text text-transparent bg-gradient-to-r from-customYellow-500 to-customOrange-500">
+            Profil
+          </span>
+    </h1>
+  </div>
 </template>
 
 <style scoped>
