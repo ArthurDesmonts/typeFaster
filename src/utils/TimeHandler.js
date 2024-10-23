3c32c7ae931fe45284d6c7ad6cb79d4e42a19cf1
arthur <arthur.desmonts@gmail.com>
2024-10-23 11:38:54 +0200

    add Doc to TimeHandler.js

--- src/utils/TimeHandler.js
+++ src/utils/TimeHandler.js
@@ -1,5 +1,11 @@
+//GLOBAL VAR : Stock the interval for the clock countdown
 let interval;
-//Count down from a given number of seconds and call a callback function every second
+
+/**
+ * This function set an interval to countdown from a given number of seconds, ticking every second.
+ * @param seconds
+ * @param callback
+ */
 export function countDownFrom(seconds, callback) {
     interval = setInterval(() => {
         if (seconds === 0) {
@@ -12,6 +18,10 @@ export function countDownFrom(seconds, callback) {
     }, 1000);
 }
 
+/**
+ * Reset the countdown
+ * @returns {void}
+ */
 export function resetCountDown() {
     if (interval) {
         clearInterval(interval);
