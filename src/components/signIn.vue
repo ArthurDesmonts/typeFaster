236f6efd46fcc71548a3dcd1ae122d15c65353a8
arthur <arthur.desmonts@gmail.com>
2024-10-24 14:03:20 +0200

    add button signIn animation for the server connexion

--- src/components/signIn.vue
+++ src/components/signIn.vue
@@ -59,6 +59,19 @@ async function postLogin() {
                      class="form-control bg-customBlue-800 rounded font-bold text-center w-full" v-model="password">
             </div>
             <button type="submit" class="btn btn-primary bg-customOrange-500 rounded text-customBlue-900 p-2">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+                <radialGradient id="a10" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)">
+                  <stop offset="0" stop-color="#000000"></stop>
+                  <stop offset=".3" stop-color="#000000" stop-opacity=".9"></stop>
+                  <stop offset=".6" stop-color="#000000" stop-opacity=".6"></stop>
+                  <stop offset=".8" stop-color="#000000" stop-opacity=".3"></stop>
+                  <stop offset="1" stop-color="#000000" stop-opacity="0"></stop>
+                </radialGradient>
+                <circle transform-origin="center" fill="none" stroke="url(#a10)" stroke-width="15" stroke-linecap="round" stroke-dasharray="200 1000" stroke-dashoffset="0" cx="100" cy="100" r="70">
+                  <animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="2" values="360;0" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform>
+                </circle>
+                <circle transform-origin="center" fill="none" opacity=".2" stroke="#000000" stroke-width="15" stroke-linecap="round" cx="100" cy="100" r="70"></circle>
+              </svg>
               Connexion
             </button>
             <p class="text-center text-customOrange-500">Pas encore inscrit ? <router-link to="/typeFaster/signUp"><span class="text-blue-500">Inscrivez-vous</span></router-link></p>
